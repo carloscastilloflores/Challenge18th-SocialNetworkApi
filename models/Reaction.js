074b400f7fabdae49defe82c826a560e147f9727
carloscastilloflores <125534814+carloscastilloflores@users.noreply.github.com>
2023-08-03 14:07:43 -0600

    Updated code

---
+++ models/Reaction.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose'); 
+const reactionSchema = require('./Reaction.js');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: mongoose.Types.ObjectId,
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max_length: 280
+        }, 
+        username: { 
+            type: String, 
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,  
+            get: function() {
+                return this.date.toLocaleString();
+        },
+        }
+    }
+);
+
+const Reaction = model('thourght', reactionSchema);
+
+module.exports = Reaction; 
