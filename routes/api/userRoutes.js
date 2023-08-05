4adbf171a743ef47a2b880d5aca13a79e86e0a9e
carloscastilloflores <125534814+carloscastilloflores@users.noreply.github.com>
2023-08-05 15:55:32 -0600

    Fixed some api endpoints for users

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -7,18 +7,21 @@ const {
   deleteUser,
   addFriend,
   deleteFriend
-} = require('../../controllers/userController.js');
+} = require('../../controllers/userController');
 
-
+// /api/users
 router.route('/')
 .get(getUsers)
+.post(createUser);
+
+// /api/users/:userId
+router.route('/:userId')
 .get(getSingleUser)
-.post(createUser)
 .put(updateUser)
 .delete(deleteUser);
 
 // /api/users/:userId/friends/:friendId
-router.route('_/:userId/friends/friendsId')
+router.route('/:userId/friends/friendsId')
 .post(addFriend)
 .delete(deleteFriend);
 
